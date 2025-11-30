e219d469134a4fabb855358b60a425c2f34c9f4b
teddylindstrom <teddy.lindstrom.work@gmail.com>
2025-11-30 11:57:23 +0100

    todo list html

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,16 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
+
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`;
+  todoListHTML += html;
+}
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML;
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
