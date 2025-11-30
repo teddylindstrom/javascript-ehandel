1db0516c4f54f646a135f69b4c3e4a7ea6933d7b
teddylindstrom <teddy.lindstrom.work@gmail.com>
2025-11-30 12:13:33 +0100

    adding tasks to the todolist by render list

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,16 +1,21 @@
 const todoList = ['make dinner', 'wash dishes'];
 
-let todoListHTML = '';
+renderTodoList();
 
-for (let i = 0; i < todoList.length; i++) {
+function renderTodoList(){
+
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
   const todo = todoList[i];
   const html = `<p>${todo}</p>`;
   todoListHTML += html;
-}
-console.log(todoListHTML);
+  }
+  console.log(todoListHTML);
 
-document.querySelector('.js-todo-list')
+  document.querySelector('.js-todo-list')
   .innerHTML = todoListHTML;
+}
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
@@ -20,4 +25,6 @@ function addTodo() {
   console.log(todoList);
 
   inputElement.value = '';
+
+  renderTodoList();
 }
